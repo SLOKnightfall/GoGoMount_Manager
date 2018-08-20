089ab5488ac33eb176fe694ad83a01418ae16001
SLOKnightfall <SLOKnightfall@gmail.com>
2018-08-19 20:13:46 -0700

    .pkgmeta update for missing dualspec library.

--- Localization/localization-enUS.lua
+++ Localization/localization-enUS.lua
@@ -4,3 +4,5 @@ local L = LibStub("AceLocale-3.0"):NewLocale("GoGoMount_Manager", "enUS", true)
 L.GLOBAL_ENABLE = "Prefered Mount"
 L.GLOBAL_EXCLUDE = "Excluded Mount"
 L.GLOBAL_CLEAR = "Left Click: Prefered Mount\nRight Click: Exclude Mount"
+L.ZONE_ENABLE = "Zone Prefered Mount"
+L.ZONE_EXCLUDE = "Zone Excluded Mount"
